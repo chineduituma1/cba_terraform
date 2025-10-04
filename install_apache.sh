692c9bc0288f1d6f08f96162fe2bdcb78a8f0f54
chineduituma1 <chineduituma1@gmail.com>
2025-10-04 11:06:25 +0100

    second commit

--- install_apache.sh
+++ install_apache.sh
@@ -3,5 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-# shellcheck disable=SC2024
-sudo echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+sudo echo "Hello World from $(hostname -f)" | /var/www/html/index.html
